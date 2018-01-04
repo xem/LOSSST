2e9de36f84a1efddab01643799e6750e10674d80
xem <maxime.euziere@gmail.com>
2018-01-04 15:41:27 +0100

    use vmin, add shadows

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 ﻿// Globals
 
 snakelength = 6;
-snakepos = [[5,2],[5,1],[5,0],[5,-1],[5,-2],[5,-3]];
+snakepos = [[7,3],[7,2],[7,1],[7,0],[7,-1],[7,-2]];
 snakeangles = [-90];
 headangle = -90;
 lastcell = null;
@@ -11,7 +11,8 @@ lastcell = null;
 for(i=0;i<6;i++){
   snake.innerHTML +=
   `<div id="snakecubemove${i}" class="snakecubemove">
-    <div id="cubesnake${i}" class="cube snakecube">
+    <div class="snakeshadow"></div>
+    <div id="snakecube${i}" class="cube snakecube">
       ${i?'':'<div class="tongue">Y</div>'}
       ${i?'<div class="u"></div>':'<div class="u eyes">👀</div>'}
       ${i?'<div class="f"></div>':'<div class="f mouth">‿</div>'}
@@ -22,9 +23,9 @@ for(i=0;i<6;i++){
   </div>`;
 }
 
-for(i=0;i<11;i++){
-  for(j=0;j<11;j++){
-    puzzle.innerHTML += '<div id=cell' + i + '-' + j + ' class="cell '+ (i<3||i>7||j<3||j>7 ? "blank" : "") + '" style="top:' + (i * 10) + 'vh;left:' + (j * 10) + 'vh"></div>';
+for(i = 0; i < 15; i++){
+  for(j = 0; j < 15; j++){
+    puzzle.innerHTML += '<div id=cell' + i + '-' + j + ' class="cell '+ (i < 5 || i > 9 || j < 5 || j > 9 ? "blank" : "") + '" style="top:' + (i * 10) + 'vmin;left:' + (j * 10) + 'vmin"></div>';
   }
 }
 
@@ -47,6 +48,7 @@ mousemove = e => {
   if(newcell.className.indexOf("cell") > -1){
     j = parseInt(newcell.style.left) / 10;
     i = parseInt(newcell.style.top) / 10;   
+    
     ok = 1;
     for(p = 0; p < snakelength - 1; p++){
       if(i == snakepos[p][0] && j == snakepos[p][1]){
@@ -92,7 +94,9 @@ mousemove = e => {
 
 movesnake = () => {
   for(i = 0; i < 6; i++){
-    window["snakecubemove" + i].style.transform = "translateX(" + (snakepos[i][1] * 10 + 1) + "vh) translateY(" + (snakepos[i][0] * 10 + 1) + "vh) translateZ(.5vh) rotateZ(" + (i ? 0 : headangle) + "deg)";
+    window["snakecubemove" + i].style.transform = "translateX(" + (snakepos[i][1] * 10 + 1) + "vmin) translateY(" + (snakepos[i][0] * 10 + 1) + "vmin) translateZ(.5vmin)";
+
+    window["snakecube" + i].style.transform = "rotateZ(" + (i ? 0 : headangle) + "deg)";
     
     cell = window["cell" + snakepos[i][0] + "-" + snakepos[i][1]];
     if(cell && cell.className.indexOf("blank") == -1){
