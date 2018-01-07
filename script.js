ce5c4115ed8e00725da00c267e3fd6abe7a3e521
xem <maxime.euziere@gmail.com>
2018-01-07 11:09:24 +0100

    pathfinding ok!

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 ﻿// Globals
-
 snakelength = 6;
-snakepos = [[7,3],[7,2],[7,1],[7,0],[7,-1],[7,-2]];
+snakepos = [[3,7],[2,7],[1,7],[0,7],[-1,7],[-2,7]];
 snakeangles = [-90];
 headangle = -90;
 lastcell = null;
 inbounds = 0;
 grabbed = 0;
 
-
-
 // Puzzles
-
 puzzles = ["0000000110011000110000000"];
 currentpuzzle = 0;
 
-
-
-
 // DOM
-
 for(i=0;i<6;i++){
   snake.innerHTML +=
   `<div id="snakecubemove${i}" class="snakecubemove">
@@ -35,164 +27,143 @@ for(i=0;i<6;i++){
   </div>`;
 }
 
-for(i = 0; i < 15; i++){
-  for(j = 0; j < 15; j++){
-    puzzle.innerHTML += '<div id=cell' + i + '-' + j + ' class="cell '+ (i < 5 || i > 9 || j < 5 || j > 9 ? "grass " : +puzzles[currentpuzzle][(i-5)*5+(j-5)] ? "black" : "") + '" style="top:' + (i * 10) + 'vmin;left:' + (j * 10) + 'vmin"></div>';
+for(x = 0; x < 15; x++){
+  for(y = 0; y < 15; y++){
+    puzzle.innerHTML += '<div data-x=' + x + ' data-y=' + y + ' id=cell' + x + '-' + y + ' class="cell '+ (x < 5 || x > 9 || y < 5 || y > 9 ? "grass " : +puzzles[currentpuzzle][(x-5)*5+(y-5)] ? "black" : "") + '" style="left:' + (x * 10) + 'vmin;top:' + (y * 10) + 'vmin"></div>';
   }
 }
 
-
-
 // Pointer events
-
 scene.ontouchmove = e => {
   e.preventDefault();
   e.stopPropagation();
-  mousemove(e.targetTouches[0]);
+  mousemove(document.elementFromPoint(e.targetTouches[0].clientX, e.targetTouches[0].clientY), 1);
 }
 
 scene.onmousemove = e => {
   e.preventDefault();
   e.stopPropagation();
-  mousemove(e);
+  mousemove(document.elementFromPoint(e.clientX, e.clientY), 1);
 }
 
-mousemove = e => {
-  var i, j, ok, x, y, headangle, cell, newcell;
-  
-  newcell = document.elementFromPoint(e.clientX, e.clientY);
-  if(newcell.classList.contains("cell")){
-    j = parseInt(newcell.style.left) / 10;
-    i = parseInt(newcell.style.top) / 10;   
-    
+mousemove = (cell, pathfinding) => {
+  var newx, newy, tilex, tiley, ok, headangle, newcell, c;
+
+  if(cell.classList.contains("cell")){
+    tilex = +cell.dataset.x;
+    tiley = +cell.dataset.y;
     ok = 1;
+    
+    // Check collisions
     for(p = 0; p < snakelength - 1; p++){
-      if(i == snakepos[p][0] && j == snakepos[p][1]){
+      if(tilex == snakepos[p][0] && tiley == snakepos[p][1]){
         ok = 0;
       }
     }
-    
-    // Advance to one of the fourth neighbours
+
+    // No collision
     if(ok){
       ok = 0;
-      if((i == snakepos[0][0] - 1 && j == snakepos[0][1])){
-        headangle = 180; // top
-        ok = grabbed = 1;
-      }
-      else if(i == snakepos[0][0] && j == snakepos[0][1] - 1){
+      
+      // Go in one of the four direct neighbors (N/S/W/E)
+      if((tilex == snakepos[0][0] - 1 && tiley == snakepos[0][1])){
         headangle = 90; // left
         ok = grabbed = 1;
       }
-      else if(i == snakepos[0][0] + 1 && j == snakepos[0][1]){
-        headangle = 0; // bottom
+      else if(tilex == snakepos[0][0] && tiley == snakepos[0][1] - 1){
+        headangle = 180; // top
         ok = grabbed = 1;
       }
-      else if(i == snakepos[0][0] && j == snakepos[0][1] + 1){
+      else if(tilex == snakepos[0][0] + 1 && tiley == snakepos[0][1]){
         headangle = -90; // right
         ok = grabbed = 1;
       }
+      else if(tilex == snakepos[0][0] && tiley == snakepos[0][1] + 1){
+        headangle = 0; // bottom
+        ok = grabbed = 1;
+      }  
       if(ok){
-        snakepos.unshift([i, j]);
+        snakepos.unshift([tilex, tiley]);
         snakeangles.unshift(headangle);
         movesnake();
+        return true;
       }
       
-      // Try to advance multiple times at once if the pointer is too quick but the path inbetween is free, without going in diagonal
-      else if(grabbed){
-        
-        //console.log(snakepos[0][0], snakepos[0][1], i, j);
-        
-        x = snakepos[0][1];
-        y = snakepos[0][0];
-        
+      // Try to go in diagonals in 2 steps
+      if((tilex == snakepos[0][0] - 1 && tiley == snakepos[0][1] - 1)){
+        return (
+          mousemove(window["cell" + (snakepos[0][0] - 1) + '-' + (snakepos[0][1])], 0)
+          || mousemove(window["cell" + (snakepos[0][0]) + '-' + (snakepos[0][1] - 1)], 0)
+        )
+      }
+      else if(tilex == snakepos[0][0] - 1 && tiley == snakepos[0][1] + 1){
+        return (
+          mousemove(window["cell" + (snakepos[0][0] - 1) + '-' + (snakepos[0][1])], 0) 
+          || mousemove(window["cell" + (snakepos[0][0]) + '-' + (snakepos[0][1] + 1)], 0)
+        )
+      }
+      else if(tilex == snakepos[0][0] + 1 && tiley == snakepos[0][1] - 1){
+        return (
+          mousemove(window["cell" + (snakepos[0][0] + 1) + '-' + (snakepos[0][1])], 0) 
+          || mousemove(window["cell" + (snakepos[0][0]) + '-' + (snakepos[0][1] - 1)], 0)
+        )
+      }
+      else if(tilex == snakepos[0][0] + 1 && tiley == snakepos[0][1] + 1){
+        return (
+          mousemove(window["cell" + (snakepos[0][0] + 1) + '-' + (snakepos[0][1])], 0) 
+          || mousemove(window["cell" + (snakepos[0][0]) + '-' + (snakepos[0][1] + 1)], 0)
+        )
+      }
+      
+      // Try to advance multiple times at once if the pointer is too quick but the path in-between is free
+      else if(grabbed && pathfinding){
+        newx = snakepos[0][0];
+        newy = snakepos[0][1];
+        ok = 1;
         for(c = 0; c < 30; c++){
-          if(x != j && y != i){
-
-            x += (j - snakepos[0][1]) / 30;
-            y += (i - snakepos[0][0]) / 30;
-              
-            ok = 1;
-            for(p = 0; p < snakelength - 1; p++){
-              if((~~x) == snakepos[p][1] && (~~y) == snakepos[p][0]){
-                ok = 0;
-              }
-            }
-              
-            if(ok && (Math.abs((~~x) - snakepos[0][1]) + Math.abs((~~y) - snakepos[0][0])) == 1){
-              ok = 0;
-              if((~~x == snakepos[0][0] - 1 && ~~y == snakepos[0][1])){
-                headangle = 180; // top
-                ok = 1;
-              }
-              else if(~~x == snakepos[0][0] && ~~y == snakepos[0][1] - 1){
-                headangle = 90; // left
-                ok = 1;
-              }
-              else if(~~x == snakepos[0][0] + 1 && ~~y == snakepos[0][1]){
-                headangle = 0; // bottom
-                ok = 1;
-              }
-              else if(~~x == snakepos[0][0] && ~~y == snakepos[0][1] + 1){
-                headangle = -90; // right
-                ok = 1;
-              }
-              
-              
-              if(ok){
-                headangle = -90;
-                snakepos.unshift([~~y, ~~x]);
-                snakeangles.unshift(headangle);
-                movesnake();
-              }
-              else {
-                break;
-              }
-            }
-            
-            else if((~~x) == snakepos[0][1] && (~~y) == snakepos[0][0]){
-              // continue
-            }
-            else {
-              break;
-            }
-          }
+            newx = snakepos[0][0] + c * (tilex - snakepos[0][0]) / 30;
+            newy = snakepos[0][1] + c * (tiley - snakepos[0][1]) / 30;
+            mousemove(window["cell" + (~~newx) + "-" + (~~newy)], 0);
         }
       }
     }
     
-    // Go back
-    else if(i == snakepos[1][0] && j == snakepos[1][1] && snakepos.length > 6){
-      
+    // Go back when hovering the cell containing the second cube of the snake
+    else if(tilex == snakepos[1][0] && tiley == snakepos[1][1] && snakepos.length > 6){
       cell = window["cell" + snakepos[0][0] + "-" + snakepos[0][1]];
       if(cell){
         cell.classList.remove("blue", "red");
       }
-      
       cell = window["cell" + snakepos[6][0] + "-" + snakepos[6][1]];
       if(cell && !cell.classList.contains("grass")){
         cell.classList.add(cell.classList.contains("black") ? "blue" : "red");
       }
-      
       snakepos.shift();
       snakeangles.shift();
       headangle = snakeangles[0];
       movesnake();
+      return true;
+    }
+    
+    // Collision
+    else {
+      return false;
     }
   }
 }
 
-
 movesnake = () => {
-  
   var i, cell;
-  
+  if(snakepos[7]){
+    cell = window["cell" + snakepos[6][0] + "-" + snakepos[6][1]];
+    if(cell){
+      cell.classList.remove("blue", "red");
+    }
+  }
   for(i = 0; i < 6; i++){
-    window["snakecubemove" + i].style.transform = "translateX(" + (snakepos[i][1] * 10 + 1) + "vmin) translateY(" + (snakepos[i][0] * 10 + 1) + "vmin) translateZ(.5vmin)";
-
+    window["snakecubemove" + i].style.transform = "translateX(" + (snakepos[i][0] * 10 + 1) + "vmin) translateY(" + (snakepos[i][1] * 10 + 1) + "vmin) translateZ(.5vmin)";
     window["snakecube" + i].style.transform = "rotateZ(" + (i ? 0 : snakeangles[0]) + "deg)";
-    
     cell = window["cell" + snakepos[i][0] + "-" + snakepos[i][1]];
-    
     if(i == 0){
       if(cell && !cell.classList.contains("grass")){
         inbounds = 1;
@@ -203,7 +174,6 @@ movesnake = () => {
       }
     }
   }
-  
   if(inbounds && document.querySelectorAll(".cell.red").length == 0){
     console.log("won");
     for(i of document.querySelectorAll(".cell.blue")){
@@ -211,15 +181,7 @@ movesnake = () => {
       i.classList.add("green");
     }
   }
-  
-  if(snakepos[7]){
-    cell = window["cell" + snakepos[6][0] + "-" + snakepos[6][1]];
-    if(cell){
-      cell.classList.remove("blue", "red");
-    }
-  }
 }
-
 movesnake();
 
 onmousedown = onmousemove = onmouseup = /*oncontextmenu =*/ ontouchstart = ontouchmove = ontouchend = onclick = ondblclick = onscroll = function(e){
